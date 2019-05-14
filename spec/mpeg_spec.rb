9ad6199044cc320217ca9869d111701b449b6dab
mokha <mokha@cisco.com>
2019-05-14 16:15:36 -0600

    fix specs

--- spec/mpeg_spec.rb
+++ spec/mpeg_spec.rb
@@ -1,9 +1,3 @@
 RSpec.describe Mpeg do
-  it "has a version number" do
-    expect(Mpeg::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  specify { expect(Mpeg::VERSION).not_to be nil }
 end
